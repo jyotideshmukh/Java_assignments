817bd7adaff651e21c77ee1ddcfe93ce7af3f6c5
Santosh Todkar <jyotipawar2003@gmail.com>
2020-08-16 17:29:42 +0530

    cat playing assignment

--- src/Assignment/PlayingCat.java
+++ src/Assignment/PlayingCat.java
@@ -1,5 +1,23 @@
 package Assignment;
 
-public class PlayingCat {
+import javax.lang.model.util.ElementScanner6;
+
+    /*
+    * cat playing assignment - if summer cat can play within temp 25 to 45
+    * cat playing if not summer but temp range is 25 to 35
+    */
+   public class PlayingCat {
+
+    public static boolean isCatPlaying(boolean summer, int temperature){
+
+        if(summer && (temperature >=25 && temperature <=45))
+            return true;
+        else if(!summer && (temperature >=25 && temperature <=35))
+            return true;
+        
+        return false;
+        
+
+    }
     
 }
