c2d4c2ada429dce00b4aa5ea136417d93c0f6652
jyotipawar2003@gmail.com <jyotipawar2003@gmail.com>
2021-05-10 17:08:29 +0530

    Added contact and mobile phone classes

--- src/Assignment/MobilePhone/MobilePhone.java
+++ src/Assignment/MobilePhone/MobilePhone.java
@@ -1,4 +1,60 @@
 package Assignment.MobilePhone;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class MobilePhone {
+
+    private ArrayList<Contact> contactList = new ArrayList<Contact>();
+
+    public boolean addContact(Contact contact){
+        if(findContact(contact)>=0){
+            System.out.println("Contact with "+contact.getName()+" already exit");
+            return false;
+        }
+        contactList.add(contact);
+        System.out.println("Contact"+ contact.getName()+ " added successfully");
+        return true;
+    }
+
+    public void editContact(String name, Contact contact){
+        int position = findContact(name);
+        if(position>=0)
+            contactList.set(position, contact);
+        else {
+            System.out.println("Contact with name " + name + " is not exist");
+        }
+    }
+
+    public void editContact(Contact oldContact, Contact contact){
+        int position = findContact(oldContact);
+        if(position>=0) {
+            contactList.set(position, contact);
+        }
+        else{
+            System.out.println("Contact with name " + oldContact.getName() + " is not exist");
+            System.out.println("Do you want to add new contact Yes/No?");
+            Scanner scanner = new Scanner(System.in);
+            String answer = scanner.nextLine();
+            if(answer.equalsIgnoreCase("Yes") || answer.equalsIgnoreCase("Y")){
+                contactList.add(contact);
+            }
+
+        }
+    }
+
+    private int findContact(Contact contact){
+         return contactList.indexOf(contact);
+    }
+
+    private int findContact(String name){
+        for(int i = 0; i < contactList.size(); i++){
+            Contact contact = contactList.get(i);
+            if(name.equalsIgnoreCase(contact.getName()){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
